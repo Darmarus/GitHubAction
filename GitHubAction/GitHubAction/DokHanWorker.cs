e0b5f4cabd2c14b8230876561a27ee29ce5917c8
Robert Osiński <robert.osinski@agemit.pl>
2026-04-30 11:22:57 +0200

    Add project files.

---
+++ GitHubAction/GitHubAction/DokHanWorker.cs
@@ -0,0 +1,32 @@
+﻿using GitHubAction;
+using Soneta.Business;
+using Soneta.Business.UI;
+using Soneta.Handel;
+using System;
+
+[assembly: Worker(typeof(DokHanWorker), typeof(DokHandlowe))]
+
+namespace GitHubAction
+{
+	public class DokHanWorker
+	{
+
+
+		// TODO -> Należy podmienić podany opis akcji na bardziej czytelny dla uzytkownika
+		[Action("DokHanWorker/ToDo", Mode = ActionMode.SingleSession | ActionMode.ConfirmSave | ActionMode.Progress)]
+		public MessageBoxInformation ToDo()
+		{
+
+			return new MessageBoxInformation("Czy wykonać operację ?")
+			{
+				Text = "Opis operacji",
+				YesHandler = () => "Operacja została zakończona",
+				NoHandler = () => "Operacja przerwana"
+			};
+
+
+		}
+	}
+
+
+}
